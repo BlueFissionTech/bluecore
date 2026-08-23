30e12aa0c26c7baa8f7a888a0f999843957bd8b3
Devon Scott <dscott@bluefission.com>
2026-08-23 15:19:20 -0400

    execute add-on registration factories once

--- src/BlueCore/Contracts/IAddOnLifecycleManager.php
+++ src/BlueCore/Contracts/IAddOnLifecycleManager.php
@@ -2,10 +2,17 @@
 
 namespace BlueFission\BlueCore\Contracts;
 
+use BlueFission\BlueCore\Registration\RegistrationPlan;
+use BlueFission\Services\Application;
+
 interface IAddOnLifecycleManager
 {
     public function install($name, bool $installDependencies = false): array;
     public function uninstall($addOnId, bool $removeDependencies = false): array;
     public function activate($addOnId): array;
     public function deactivate($addOnId): array;
+    public function loadActivatedAddOns(
+        ?Application $application = null,
+        ?RegistrationPlan $plan = null
+    ): array;
 }
